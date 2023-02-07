28748924c5d1bad0483ca7f3f7ab97a0ff674b12
fahmidyt <fmidyt@gmail.com>
2023-02-08 06:02:06 +0700

    refactor: meta description

--- app/head.tsx
+++ app/head.tsx
@@ -7,7 +7,7 @@ export default function Head() {
         <title>Fahmidyt.</title>
         <meta
           name="description"
-          content="A playground to explore new Next.js 13 app directory features such as nested layouts, instant loading states, streaming, and component level data fetching."
+          content="Welcome to my personal website. Here's you can find basicly what of me is. happy browsing!"
         />
       </>
     );
